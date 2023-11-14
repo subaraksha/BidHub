fb79ad3186ae1175f8175f40077663aff2b7f880
subaraksha <87661019+subaraksha@users.noreply.github.com>
2023-11-14 19:47:00 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js";
+import cors from "cors";
+import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+
+dotenv.config();
+connectDB();
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+app.use("/api/v1/auth", authRoutes);
+// app.use("/api/v1/auth/product", authproductRoutes);
+app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/product", productRoutes);
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to Bidhub</h1>");
+});
+
+const PORT = process.env.PORT || 3005;
+
+app.listen(PORT, () => {
+  console.log(
+    `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
+      .white
+  );
+});
